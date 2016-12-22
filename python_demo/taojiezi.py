cfa47dba3348b56faee7c26858f3a4d7ad9507a5
jitianze <978941570@qq.com>
2016-12-22 22:33:43 +0800

    简单的扫描器实现

---
+++ python_demo/taojiezi.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import socket
+
+#socket.socket() 此为创建套接字的方法
+print socket.gethostname() #获取主机名
+print socket.gethostbyname(socket.gethostname()) #获取电脑ip
+print socket.gethostbyname_ex(socket.gethostname())
+s= socket.socket()
+host="www.baidu.com"
+port=443
+s.connect((host,port))
+s.send("xuziyan")
+baner=s.recv(1024)
+if baner:
+	print baner
